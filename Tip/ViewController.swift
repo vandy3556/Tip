3b8d4df3cefadf06758824a74b7336b4a1c865b2
CHRISTOPHER VANDERGRIFF <rl239129@rlas-macair-38.local>
2018-11-14 10:36:27 -0600

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by CHRISTOPHER VANDERGRIFF on 11/14/18.
+//  Copyright © 2018 CHRISTOPHER VANDERGRIFF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
